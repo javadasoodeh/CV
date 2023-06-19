0a413633cfdf63e43bb0b871d4aac43c6ccc84e2
Javad <javadasoodeh73@gmail.com>
2023-06-19 22:04:30 +0430

    add Scale-Invariant Feature Transform

---
+++ opencv_basic/sift.py
@@ -0,0 +1,18 @@
+import cv2
+
+# Load the input image
+img = cv2.imread('HBD.jpg')
+
+# Initialize the SIFT detector
+sift = cv2.SIFT_create()
+
+# Detect keypoints and compute descriptors
+keypoints, descriptors = sift.detectAndCompute(img, None)
+
+# Draw keypoints on the input image
+img_with_keypoints = cv2.drawKeypoints(img, keypoints, None)
+
+# Display the input image with keypoints
+cv2.imshow('Input image with keypoints', img_with_keypoints)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
